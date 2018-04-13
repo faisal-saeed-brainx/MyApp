1bfc51104f03c499c2f90875c19da35367cf402f
Faisal Saeed <faisal.saeed@brainxtech.com>
2018-04-13 11:30:16 +0500

    First Commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="jumbotron text-center">
+	<h1>Welcome to Laravel</h1>
+	<p>This is my Index Page.</p>
+</div>
+
+@endsection
---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<a href="/posts" class="btn btn-primary">Back</a>
+<h1>Add New Post</h1>
+{!! Form::open(['action' => 'PostsController@store','method'=>'POST']) !!}
+    <div>
+    	{{Form::label('title','Title')}}
+    	{{Form::text('title','',['class'=>'form-control', 'placeholder'=>'Title'])}}
+    </div>
+
+    <div>
+    	{{Form::label('body','Body')}}
+    	{{Form::textarea('body','',['class'=>'form-control', 'placeholder'=>'Body Text'])}}
+    </div>
+    {{Form::submit('Submit',['class'=>'btn btn-primary'])}}
+{!! Form::close() !!}
+
+@endsection
